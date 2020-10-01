3db91e6e3b0a193a4f4b366499ca9ceeb691d541
Marco S <slow@madsn.wtf>
2020-10-01 23:54:54 +0200

    added timer session updates

--- update.php
+++ update.php
@@ -108,96 +108,110 @@ header("location:dash.php?q=0");
 }
 }
 
+/* Update quiz timer Session attribute */
+if(@$_GET['q'] == 'updateTimer')
+{
+  $timerSecs = intval(stripslashes(strip_tags($_GET['timeRemaining'])));
+  $_SESSION['quizTimeRemaining'] = $timerSecs;
+}
+
 //quiz start
-if(@$_GET['q']== 'quiz' && @$_GET['step']== 2) {
-$eid=@$_GET['eid'];
-$sn=@$_GET['n'];
-$total=@$_GET['t'];
-$ans=$_POST['ans'];
-$qid=@$_GET['qid'];
-$q=mysqli_query($con,"SELECT * FROM answer WHERE qid='$qid' " );
-while($row=mysqli_fetch_array($q) )
+if(@$_GET['q']== 'quiz' && @$_GET['step']== 2) 
 {
-$ansid=$row['ansid'];
-}
-if($ans == $ansid)
-{
-$q=mysqli_query($con,"SELECT * FROM quiz WHERE eid='$eid' " );
-while($row=mysqli_fetch_array($q) )
-{
-$sahi=$row['sahi'];
-}
-if($sn == 1)
-{
-$q=mysqli_query($con,"INSERT INTO history VALUES('$email','$eid' ,'0','0','0','0',NOW())")or die('Error');
-}
-$q=mysqli_query($con,"SELECT * FROM history WHERE eid='$eid' AND email='$email' ")or die('Error115');
+  $eid=@$_GET['eid'];
+  $sn=@$_GET['n'];
+  $total=@$_GET['t'];
+  $ans=$_POST['ans'];
+  $qid=@$_GET['qid'];
 
-while($row=mysqli_fetch_array($q) )
-{
-$s=$row['score'];
-$r=$row['sahi'];
-}
-$r++;
-$s=$s+$sahi;
-$q=mysqli_query($con,"UPDATE `history` SET `score`=$s,`level`=$sn,`sahi`=$r, date= NOW()  WHERE  email = '$email' AND eid = '$eid'")or die('Error124');
+  $_SESSION['quizTimeRemaining'] = intval(stripslashes(strip_tags($_POST['timeRemaining'])));
 
-}
-else
-{
-$q=mysqli_query($con,"SELECT * FROM quiz WHERE eid='$eid' " )or die('Error129');
+  $q=mysqli_query($con,"SELECT * FROM answer WHERE qid='$qid' " );
+  while($row=mysqli_fetch_array($q) )
+  {
+    $ansid=$row['ansid'];
+  }
 
-while($row=mysqli_fetch_array($q) )
-{
-$wrong=$row['wrong'];
-}
-if($sn == 1)
-{
-$q=mysqli_query($con,"INSERT INTO history VALUES('$email','$eid' ,'0','0','0','0',NOW() )")or die('Error137');
-}
-$q=mysqli_query($con,"SELECT * FROM history WHERE eid='$eid' AND email='$email' " )or die('Error139');
-while($row=mysqli_fetch_array($q) )
-{
-$s=$row['score'];
-$w=$row['wrong'];
-}
-$w++;
-$s=$s-$wrong;
-$q=mysqli_query($con,"UPDATE `history` SET `score`=$s,`level`=$sn,`wrong`=$w, date=NOW() WHERE  email = '$email' AND eid = '$eid'")or die('Error147');
-}
-if($sn != $total)
-{
-$sn++;
-header("location:account.php?q=quiz&step=2&eid=$eid&n=$sn&t=$total")or die('Error152');
-}
-else if( $_SESSION['key']!='vinitshahdeo')
-{
-$q=mysqli_query($con,"SELECT score FROM history WHERE eid='$eid' AND email='$email'" )or die('Error156');
-while($row=mysqli_fetch_array($q) )
-{
-$s=$row['score'];
-}
-$q=mysqli_query($con,"SELECT * FROM rank WHERE email='$email'" )or die('Error161');
-$rowcount=mysqli_num_rows($q);
-if($rowcount == 0)
-{
-$q2=mysqli_query($con,"INSERT INTO rank VALUES('$email','$s',NOW())")or die('Error165');
-}
-else
-{
-while($row=mysqli_fetch_array($q) )
-{
-$sun=$row['score'];
-}
-$sun=$s+$sun;
-$q=mysqli_query($con,"UPDATE `rank` SET `score`=$sun ,time=NOW() WHERE email= '$email'")or die('Error174');
-}
-header("location:account.php?q=result&eid=$eid");
-}
-else
-{
-header("location:account.php?q=result&eid=$eid");
-}
+  if($ans == $ansid)
+  {
+    $q=mysqli_query($con,"SELECT * FROM quiz WHERE eid='$eid' " );
+    while($row=mysqli_fetch_array($q) )
+    {
+      $sahi=$row['sahi'];
+    }
+    if($sn == 1)
+    {
+      $q=mysqli_query($con,"INSERT INTO history VALUES('$email','$eid' ,'0','0','0','0',NOW())")or die('Error');
+    }
+
+    $q=mysqli_query($con,"SELECT * FROM history WHERE eid='$eid' AND email='$email' ")or die('Error115');
+
+    while($row=mysqli_fetch_array($q) )
+    {
+      $s=$row['score'];
+      $r=$row['sahi'];
+    }
+    $r++;
+    $s=$s+$sahi;
+    $q=mysqli_query($con,"UPDATE `history` SET `score`=$s,`level`=$sn,`sahi`=$r, date= NOW()  WHERE  email = '$email' AND eid = '$eid'")or die('Error124');
+  }
+  else
+  {
+    $q=mysqli_query($con,"SELECT * FROM quiz WHERE eid='$eid' " )or die('Error129');
+
+    while($row=mysqli_fetch_array($q) )
+    {
+      $wrong=$row['wrong'];
+    }
+    if($sn == 1)
+    {
+      $q=mysqli_query($con,"INSERT INTO history VALUES('$email','$eid' ,'0','0','0','0',NOW() )")or die('Error137');
+    }
+    $q=mysqli_query($con,"SELECT * FROM history WHERE eid='$eid' AND email='$email' " )or die('Error139');
+    while($row=mysqli_fetch_array($q) )
+    {
+      $s=$row['score'];
+      $w=$row['wrong'];
+    }
+    $w++;
+    $s=$s-$wrong;
+    $q=mysqli_query($con,"UPDATE `history` SET `score`=$s,`level`=$sn,`wrong`=$w, date=NOW() WHERE  email = '$email' AND eid = '$eid'")or die('Error147');
+  }
+  if($sn != $total)
+  {
+    $sn++;
+    header("location:account.php?q=quiz&step=2&eid=$eid&n=$sn&t=$total")or die('Error152');
+  }
+  else if( $_SESSION['key']!='vinitshahdeo')
+  {
+    $q=mysqli_query($con,"SELECT score FROM history WHERE eid='$eid' AND email='$email'" )or die('Error156');
+    while($row=mysqli_fetch_array($q) )
+    {
+      $s=$row['score'];
+    }
+
+    $q=mysqli_query($con,"SELECT * FROM rank WHERE email='$email'" )or die('Error161');
+    $rowcount=mysqli_num_rows($q);
+
+    if($rowcount == 0)
+    {
+      $q2=mysqli_query($con,"INSERT INTO rank VALUES('$email','$s',NOW())")or die('Error165');
+    }
+    else
+    {
+      while($row=mysqli_fetch_array($q) )
+      {
+        $sun=$row['score'];
+      }
+      $sun=$s+$sun;
+      $q=mysqli_query($con,"UPDATE `rank` SET `score`=$sun ,time=NOW() WHERE email= '$email'")or die('Error174');
+    }
+    header("location:account.php?q=result&eid=$eid");
+  }
+  else
+  {
+    header("location:account.php?q=result&eid=$eid");
+  }
 }
 
 //restart quiz
